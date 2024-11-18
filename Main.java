012906418b9cbaf02852d6c5594ee82d1d4ead50
Awesometimei <dtnguyen194@student.hbuhsd.edu>
2024-11-18 10:17:52 -0800

    danny

--- Main.java
+++ Main.java
@@ -4,22 +4,46 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Make sure to test your methods here
+    printNTimes("code", 3);
+    printNums();
+    uprightNumberTriangle(5);
   }
 
   public static void printNTimes(String word, int N)
   {
-    // code solution here
+    for (int i = 0; i < word.length(); i++)
+    {
+      String str = word.substring(i, i+1);
+      for (int i2 = 0; i2 < N; i2++)
+      {
+        System.out.print(str);
+      }
+    }
+    System.out.println("");
   }
 
   public static void printNums()
   {
-    // code solution here
+    for (int i = 10; i > 0; i--)
+    {
+      for (int n = 0; n < i; n++)
+      {
+        System.out.print(i + " ");
+      }
+      System.out.println("");
+    }
   }
 
   public static void uprightNumberTriangle(int N)
   {
-    // code solution here
+    for (int i = 1; i <= N; i++)
+    {
+      for (int j = 1; j <= i; j++)
+      {
+        System.out.print(j + " ");
+      }
+      System.out.println(" ");
+    }
   }
 
   public static void starTree()
